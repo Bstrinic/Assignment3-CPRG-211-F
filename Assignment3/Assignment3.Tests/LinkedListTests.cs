4611316b7900477ddaf0f317b0e7c6ee094f9515
Bstrinic <BogdanStrinic003@gmail.com>
2024-03-31 15:26:46 -0600

    Created setup and first check to see if the list is empty.

---
+++ Assignment3/Assignment3.Tests/LinkedListTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+[TestFixture]
+public class LinkedListTests
+{
+	private LinkedList<int> linkedlist;
+
+	[SetUp]
+	public void SetUp()
+	{
+		// Used to initialize the LinkedList before each test case
+		linkedlist = new LinkedList<int>();
+	}
+
+	[Test]
+	public void ListIsEmpty()
+	{
+		// Using Assert to check if the list is empty
+		Assert.IsEmpty(linkedlist);
+	}
+
+	[Test]
+	public void PrependItem()
+	{
+		linkedlist.AddFirst(1);
+		Assert.AreEqual(1, linkedlist.First.Value);
+	}
+
+
+}
