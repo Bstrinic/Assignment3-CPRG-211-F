135ce67317f43d4dec027b3cb454065c6ba4b491
Bstrinic <BogdanStrinic003@gmail.com>
2024-04-02 10:34:33 -0600

    made changes to file system in order to allow the creation of a Nunit project

--- Assignment3/Assignment3.Tests/LinkedListTests.cs
+++ Assignment3/Assignment3.Tests/LinkedListTests.cs
@@ -1,99 +1,110 @@
 ﻿using System;
-using System.Security.Permissions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-[TestFixture]
-public class LinkedListTests
+namespace Assignment3.Tests
 {
-	private LinkedList<int> linkedlist;
+    [TestFixture]
+    public class LinkedListTests
+    {
+        // Using polymorphism to create a global linked list variable
+        private LinkedList<int> linkedlist;
 
-	[SetUp]
-	public void SetUp()
-	{
-		// Used to initialize the LinkedList before each test case
-		linkedlist = new LinkedList<int>();
-	}
+        [SetUp]
+        public void SetUp()
+        {
+            // Used to initialize the LinkedList before each test case
+            linkedlist = new LinkedList<int>();
+        }
 
-	[Test]
-	public void ListIsEmpty()
-	{
-		// Using Assert to check if the list is empty
-		Assert.IsEmpty(linkedlist);
-	}
+        [TearDown]
+        public void TearDown()
+        {
+            // Executes after each test method
+            linkedlist = null;
+        }
 
-	[Test]
-	public void PrependItem()
-	{
-		linkedlist.AddFirst(1);
-		Assert.AreEqual(1, linkedlist.First.Value); // Checking to see if the item was added to the beginning 
-	}
+        [Test]
+        public void ListIsEmpty()
+        {
+            // Using .IsEmpty to check if the list is empty
+            Assert.IsEmpty(linkedlist);
+        }
 
-	[Test]
-	public void AppendItem()
-	{
-		linkedlist.AddLast(1);
-		Assert.AreEqual(1, linkedlist.Last.Value); // Checking to see if the item was added to the end
-	}
+        [Test]
+        public void PrependItem()
+        {
+            linkedlist.AddFirst(1);
+            Assert.AreEqual(1, linkedlist.First.Value); // Checking to see if the item was added to the beginning 
+        }
 
-	// To check if the item is added to a specific index in the list
-	[Test]
-	public void InsertItemAtIndex()
-	{
-		linkedlist.AddFirst(1);
-		linkedlist.AddAfter(linkedlist.First, 2);
-		Assert.AreEqual(2, linkedlist.Last.Value);
-	}
+        [Test]
+        public void AppendItem()
+        {
+            linkedlist.AddLast(1);
+            Assert.AreEqual(1, linkedlist.Last.Value); // Checking to see if the item was added to the end
+        }
 
-	[Test]
-	public void ReplaceItem()
-	{
-		linkedlist.AddFirst(1);
-		var node = linkedlist.Find(1);
-		linkedlist.AddAfter(node, 2);
-		linkedlist.Remove(node); 
-		Assert.AreEqual(2, linkedlist.First.Value);
-	}
+        // To check if the item is added to a specific index in the list
+        [Test]
+        public void InsertItemAtIndex()
+        {
+            linkedlist.AddFirst(1);
+            linkedlist.AddAfter(linkedlist.First, 2);
+            Assert.AreEqual(2, linkedlist.Last.Value);
+        }
 
-	[Test]
-	public void DeleteItemFromBeginning()
-	{
-		linkedlist.AddFirst(1);
-		//Removes the first item in the list
-		linkedlist.RemoveFirst();
-		// Verifies that the list is empty 
-		Assert.IsEmpty(linkedlist);
-	}
+        [Test]
+        public void ReplaceItem()
+        {
+            linkedlist.AddFirst(1);
+            var node = linkedlist.Find(1);
+            linkedlist.AddAfter(node, 2);
+            linkedlist.Remove(node);
+            Assert.AreEqual(2, linkedlist.First.Value);
+        }
 
-	[Test]
-	public void DeleteItemFromEnd() 
-	{
-		linkedlist.AddLast(1);
-		// Removes the last item in the list
-		linkedlist.RemoveLast();
-		// Verify that the list is empty
-		Assert.IsEmpty(linkedlist);
-	}
+        [Test]
+        public void DeleteItemFromBeginning()
+        {
+            linkedlist.AddFirst(1);
+            //Removes the first item in the list
+            linkedlist.RemoveFirst();
+            // Verifies that the list is empty 
+            Assert.IsEmpty(linkedlist);
+        }
 
-	[Test]
-	public void DeleteFromMiddle() { 
-		linkedlist.AddFirst(1);
-		linkedlist.AddLast(3);
-		var middleNode = linkedlist.AddBefore(linkedlist.Last, 2);
-		linkedlist.Remove(middleNode);
-		Assert.AreEqual(2, linkedlist.Count);
-		Assert.IsFalse(linkedlist.Contains(2));
-	}
+        [Test]
+        public void DeleteItemFromEnd()
+        {
+            linkedlist.AddLast(1);
+            // Removes the last item in the list
+            linkedlist.RemoveLast();
+            // Verify that the list is empty
+            Assert.IsEmpty(linkedlist);
+        }
 
-	[Test]
-	public void FindAndRetrieveItem()
-	{
-		linkedlist.AddFirst(1);
-		var foundNode = linkedlist.Find(1);
-		Assert.IsNotNull(foundNode);
-		Assert.AreEqual(1, foundNode.Value);
-	}
+        [Test]
+        public void DeleteFromMiddle()
+        {
+            linkedlist.AddFirst(1);
+            linkedlist.AddLast(3);
+            var middleNode = linkedlist.AddBefore(linkedlist.Last, 2);
+            linkedlist.Remove(middleNode);
+            Assert.AreEqual(2, linkedlist.Count);
+            Assert.IsFalse(linkedlist.Contains(2));
+        }
+
+        [Test]
+        public void FindAndRetrieveItem()
+        {
+            linkedlist.AddFirst(1);
+            var foundNode = linkedlist.Find(1);
+            Assert.IsNotNull(foundNode);
+            Assert.AreEqual(1, foundNode.Value);
+        }
+    }
 }
-
-// Tests are not working for some reason
